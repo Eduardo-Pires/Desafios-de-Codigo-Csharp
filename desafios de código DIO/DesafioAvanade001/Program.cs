1195e3ef56ea9bc1108a002e7eebb595ce2374b4
Eduardo Pires <94654833+Eduardo-Pires@users.noreply.github.com>
2024-05-02 22:58:47 -0300

    desafio DIO Avenade 1: parte 1 concluída

--- desafios de código DIO/DesafioAvanade001/Program.cs
+++ desafios de código DIO/DesafioAvanade001/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        int saldoTotal = int.Parse(Console.ReadLine());
+        
+
+        int valorSaque = int.Parse(Console.ReadLine());
+        
+        //TODO: Criar as condições necessárias para impressão da saída, vide tabela de exemplos.
+
+        if (saldoTotal >= valorSaque)
+        {
+            Console.WriteLine($"Saque realizado com sucesso. Novo saldo: {saldoTotal - valorSaque}");
+        }
+        else
+        {
+            Console.WriteLine("Saldo insuficiente. Saque nao realizado!");
+        }
+    }
+}
