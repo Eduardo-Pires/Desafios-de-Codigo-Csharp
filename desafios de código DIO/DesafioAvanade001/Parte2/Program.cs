d293ca196e0eab6db47249e3c237218520fec891
Eduardo Pires <94654833+Eduardo-Pires@users.noreply.github.com>
2024-05-03 02:27:40 -0300

    desafio DIO Avenade 1: finalizado

--- desafios de código DIO/DesafioAvanade001/Parte2/Program.cs
+++ desafios de código DIO/DesafioAvanade001/Parte2/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Lista para armazenar os itens
+        List<string> itens = new List<string>();
+
+        // TODO: Solicite os itens ao usuário
+        for (int i = 0; i < 3; i++)
+        {
+            itens.Add(Console.ReadLine());
+           
+        }
+
+        // Exibe a lista de itens
+        Console.WriteLine("Lista de itens:");
+        foreach (string item in itens)
+        {
+            Console.WriteLine($"- {item}");
+        }
+    }
+}
