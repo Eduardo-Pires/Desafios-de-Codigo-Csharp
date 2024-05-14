5adbd8176857c09144dc1598aece7410183f777a
Eduardo Pires <94654833+Eduardo-Pires@users.noreply.github.com>
2024-05-13 23:15:27 -0300

    desafio DIO Avenade 2: parte 2 concluída

---
+++ desafios de código DIO/DesafioAvanade002/Parte2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static bool VerificarAtributo(string atributo, int valorMinimo, int valorMaximo, int valorAtributo)
+    {
+        if(valorAtributo > valorMaximo || valorAtributo < valorMinimo)
+        {
+            Console.WriteLine("O valor do atributo está fora do intervalo especificado");
+            return false;
+        }
+        else
+        {
+            Console.WriteLine("O valor do atributo está dentro do intervalo especificado");
+            return true;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        string atributo = Console.ReadLine();
+
+        int valorMinimo = int.Parse(Console.ReadLine());
+
+        int valorMaximo = int.Parse(Console.ReadLine());
+
+        int valorAtributo = int.Parse(Console.ReadLine());
+
+        VerificarAtributo(atributo, valorMinimo, valorMaximo, valorAtributo);
+    }
+}
